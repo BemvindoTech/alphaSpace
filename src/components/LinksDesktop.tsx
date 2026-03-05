e2dd52da5a4ce1ab840f018c458ababf4014f185
BemvindoTech <bienvenu.mendy@univ-thies.sn>
2026-03-05 18:59:58 +0000

    Mise à jour du Readme.md

--- src/components/LinksDesktop.tsx
+++ src/components/LinksDesktop.tsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom"
 
 export const LinksDesktop = () => {
   return (
-    <div className="hidden w-full lg:flex gap-x-[5rem] justify-center items-center">
+    <div className="hidden  text-white w-full lg:flex gap-x-[5rem] justify-center items-center">
         {links.map((link)=>{
             const {ref, label} = link as Link;
             return <NavLink to={ref} className={({isActive}) => `capitalize tracking-wide ${isActive? "underline text-xl": ""}`}>{label}</NavLink>
         })}
     </div>
   )
-}
+} 
--- src/components/LinksMobile.tsx
+++ src/components/LinksMobile.tsx
@@ -1,5 +1,28 @@
+import { links, type Link } from "@/utils/links"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu"
+import { NavLink } from "react-router-dom";
+import { AlignLeft } from "lucide-react";
+import { Button } from "./ui/button";
+
 export const LinksMobile = () => {
   return (
-    <div>LinksMobile</div>
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild className="lg:hidden">
+            <Button variant="outline" size="icon">
+                <AlignLeft />
+            </Button>
+            </DropdownMenuTrigger>
+        <DropdownMenuContent className="lg:hidden " align="start" sideOffset={10}>
+            {links.map((link)=>{
+            const {ref, label} = link as Link;
+            return (
+            <DropdownMenuItem key={label}>
+                <NavLink to={ref} className={({isActive}) => `capitalize tracking-wide font-light ${isActive? " text-primary": ""}`}>
+                    {label}
+                </NavLink>
+            </DropdownMenuItem>
+        )})}
+        </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
