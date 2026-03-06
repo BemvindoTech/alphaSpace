1f2aba221d9ba355f372f8e667d98be84ce0fe40
BemvindoTech <bienvenu.mendy@univ-thies.sn>
2026-03-06 20:00:55 +0000

    Implémenter la page Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,35 @@
-export const Footer = () => {
+import type { ReactNode } from "react"
+import { Link } from "react-router-dom"
+export const Footer = (): ReactNode => {
   return (
-    <footer className="test">Footer</footer>
+    <footer className="bg-black text-white">
+      <div className="align-element min-h-[15vh] flex flex-col justify-center items-center ">
+        <p className="my-2">-2026-</p>
+        <p className="mars-font text-2xl my-4 text-center">alphaSpace. by BemvindoTech</p>
+        <p className="underline">ressources:</p>
+        <p className="text-center">
+          <Link to={"https://www.nasa.gov"} target="_blank">Nasa</Link> | 
+          <Link to={"https://www.api.nasa.gov"} target="_blank"> Nasa API</Link> | 
+          <Link to={"https://www.youtube.com/@NASA/videos"} target="_blank"> Nasa youtube channel</Link>
+        </p>
+        <p>
+          <Link to={"https://webbtelescope.org/home"} target="_blank">JWST</Link>
+          <Link to={"https://jwstapi.com"} target="_blank">JWST API</Link>
+        </p>
+        <p>
+          <Link to={"https://www.esa.int/"} target="_blank">ESA</Link>
+        </p>
+        <p>
+          <Link to={"https://www.spacex.com"} target="_blank">SpaceX</Link> | 
+          <Link to={"https://www.api.spacexdata.com/v3/"} target="_blank"> SpaceX API</Link>
+        </p>
+        <p>
+          <Link to={"https://www.datastro.eu/pages/home"} target="_blank">Datastro API</Link>
+        </p>
+        <p className="mb-8">
+          <Link to={"https://www.spaceflightnewsapi.net"} target="_blank">SNAPI API</Link>
+        </p>
+      </div>
+    </footer>
   )
 }
--- src/components/LinksDesktop.tsx
+++ src/components/LinksDesktop.tsx
@@ -6,7 +6,7 @@ export const LinksDesktop = () => {
     <div className="hidden  text-white w-full lg:flex gap-x-[5rem] justify-center items-center">
         {links.map((link)=>{
             const {ref, label} = link as Link;
-            return <NavLink to={ref} className={({isActive}) => `capitalize tracking-wide ${isActive? "underline text-xl": ""}`}>{label}</NavLink>
+            return <NavLink key={link.label} to={ref} className={({isActive}) => `capitalize tracking-wide ${isActive? "underline text-xl": ""}`}>{label}</NavLink>
         })}
     </div>
   )
